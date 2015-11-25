d235b9fc22caeb80d09ebbe21ae05abb887027e7
john flood <john flood>
2015-11-25 12:32:53 +0000

    Scene Changes and WallWalking Script

---
+++ GameDevProj_UnityFolder/Assets/Scripts/HideWhenClicked.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HideWhenClicked : MonoBehaviour {
+
+
+	// CLICK Wasn't working changed to keyboard input
+	void Start()
+	{
+			gameObject.SetActive (true);
+	}
+
+
+	void Update()
+	{
+		if(Input.GetKeyDown("return"))
+			gameObject.SetActive (false);
+	}
+}
