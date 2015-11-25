ec018e6b6429a088a59269e40cf397cad20c9076
alan-leech <b00058876@student.itb.ie>
2015-11-25 14:23:52 +0000

    Guard in main scene

---
+++ GameDevProj_UnityFolder/Assets/Scripts/throwStone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwStone : MonoBehaviour {
+
+	public GameObject stone;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			Instantiate(stone, transform.position+(transform.forward*6), transform.rotation);
+
+		}
+
+	}
+}
