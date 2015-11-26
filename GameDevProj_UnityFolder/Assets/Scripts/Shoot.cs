902f36cc9434c3e00fb9ad7388f8b303d81c8000
PaulCairns <B00075124@student.itb.ie>
2015-11-26 17:45:25 +0000

    bullets

---
+++ GameDevProj_UnityFolder/Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	
+	public Rigidbody projectile;
+	public float speed = 20;
+	public GameObject bullet;
+	public float time = 2;
+	
+	
+	void Update()
+	{
+		if(Input.GetButton("Fire1"))
+		{			
+			Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation)as Rigidbody;
+			instantiatedProjectile.velocity = transform.TransformDirection (new Vector3(0 ,0,speed));
+					
+		}
+		
+	}
+	
+}
