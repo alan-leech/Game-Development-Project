d416521761dcdb3fcf388e3c13d5468b3a14176b
john flood <john flood>
2015-11-27 12:58:03 +0000

    Added Guard

--- GameDevProj_UnityFolder/Assets/Scripts/GuardCharacterController.cs
+++ GameDevProj_UnityFolder/Assets/Scripts/GuardCharacterController.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace UnityStandardAssets.Characters.ThirdPerson
-{
 	[RequireComponent(typeof (NavMeshAgent))]
 	[RequireComponent(typeof (ThirdPersonCharacter))]
 	public class GuardCharacterController : MonoBehaviour
@@ -10,14 +8,15 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			public NavMeshAgent agent { get; private set; } // the navmesh agent required for the path finding
 			public ThirdPersonCharacter character { get; private set; } // the character we are controlling
 			public Transform target; // target to aim for
-			//public GameObject spawn; // initial position
+			public GameObject spawn; // initial position
 			
 			// Use this for initialization
 			private void Start()
 			{
 				// get the components on the object we need ( should not be null due to require component so no need to check )
 				agent = GetComponentInChildren<NavMeshAgent>();
-				character = GetComponent<ThirdPersonCharacter>();
+				//character = GetComponent<ThirdPersonCharacter>();
+				character = GameObject.FindWithTag("Guard").GetComponent<ThirdPersonCharacter>();
 				
 				agent.updateRotation = false;
 				agent.updatePosition = true;
@@ -42,12 +41,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 					// We still need to call the character's move function, but we send zeroed input as the move param.
 					character.Move(Vector3.zero, false, false);
 
-					this.target = GameObject.FindWithTag("patrol_point_2").transform;
+					this.target = spawn.transform;
 				}
 				
 				
 				Vector3 fwd = transform.TransformDirection (Vector3.forward); //if player moves infront of guard chase player
-				if (Physics.Raycast (transform.position, fwd, 15)) {
+				if (Physics.Raycast (transform.position, fwd, 10)) {
 					this.target = GameObject.FindWithTag ("Player").transform; 
 				}
 
@@ -72,25 +71,45 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			
 			
 			public void OnTriggerEnter(Collider col){
-				
-				if(col.gameObject.name == "Patrol_Point_1"){
-					this.target = GameObject.FindWithTag("patrol_point_2").transform;
+
+			if (col.gameObject.name == "Patrol_Point_1" | col.gameObject.name == "Patrol_Point_2") 
+			{
+
+				if (col.gameObject.name == "Patrol_Point_1") 
+				{
+					this.target = GameObject.FindWithTag ("patrol_point_2").transform;
+				} 
+				else if (col.gameObject.name == "Patrol_Point_2") 
+				{
+					this.target = GameObject.FindWithTag ("patrol_point_1").transform;
 				}
+			}
+
+			else if (col.gameObject.name == "Patrol_Point_3" | col.gameObject.name == "Patrol_Point_4") 
+			
+			{
 				
-				else if(col.gameObject.name == "Patrol_Point_2"){
-					this.target = GameObject.FindWithTag("patrol_point_1").transform;
+				if (col.gameObject.name == "Patrol_Point_3") 
+				{
+					this.target = GameObject.FindWithTag ("patrol_point_4").transform;
+				} 
+				else if (col.gameObject.name == "Patrol_Point_4") 
+				{
+					this.target = GameObject.FindWithTag ("patrol_point_3").transform;
 				}
+			}
 				
-				else if(col.gameObject.name == "Cylinder"){
-					this.target = GameObject.FindWithTag("Player").transform;
-				}
-				
-				else if(col.gameObject.name == "Stone(Clone)"){
-					this.target = GameObject.FindWithTag("stone").transform;
-				}
+			else if(col.gameObject.name == "Cylinder")
+			{
+				this.target = GameObject.FindWithTag("Player").transform;
+			}
+			
+			else if(col.gameObject.name == "Stone(Clone)")
+			{
+				this.target = GameObject.FindWithTag("stone").transform;
+			}
 				
 			}
 			
 			
-		}
-}
+}//end of class
